8da45c311927850abeddcc9a0f27fd28247bb6b8
thelastpawn009 <72349502+thelastpawn009@users.noreply.github.com>
2021-03-09 17:35:05 +0530

    Update Create.js

--- src/Create.js
+++ src/Create.js
@@ -11,7 +11,7 @@ const Create = () => {
         e.preventDefault();
         const blog={title,body,author};
         setPending(true);
-        fetch('http://localhost:8000/blogs', {
+        fetch('https://my-json-server.typicode.com/thelastpawn009/fakejson', {
             method:'POST',
             headers:{
                 "Content-Type":"application/json"
