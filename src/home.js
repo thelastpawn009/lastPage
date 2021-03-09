2463c3ba39fe58afaac7cae98015f111bb93edc1
thelastpawn009 <72349502+thelastpawn009@users.noreply.github.com>
2021-03-09 19:24:06 +0530

    Update home.js

--- src/home.js
+++ src/home.js
@@ -4,7 +4,7 @@ import useFetch from "./usefetch";
 
 const Home = () => {
     
-   const {data:blogs,error,ispending}=useFetch('https://github.com/thelastpawn009/fakejson/blob/[main|master]/db.json');
+   const {data:blogs,error,ispending}=useFetch('https://gist.githubusercontent.com/thelastpawn009/ca9d9d824e290b87286dd4056978f1bf/raw/5dacd753df58839b5c0b55b87cb4e891e8ba3bd9/db.json');
     
     return ( 
         <div className="home">
